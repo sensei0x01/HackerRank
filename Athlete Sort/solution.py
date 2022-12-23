6ceb89e6f731454f9902588736c7c6906cdb1ce8
naderly <ahmednaderr406@gmail.com>
2022-12-24 00:06:02 +0200

    update solution.py

--- Athlete Sort/solution.py
+++ Athlete Sort/solution.py
@@ -1,4 +1,4 @@
-
+# Python 3.x 
 
 import math
 import os
@@ -9,6 +9,7 @@ import sys
 
 
 if __name__ == '__main__':
+    #preper the input n,m, attribute values and K.
     nm = input().split()
 
     n = int(nm[0])
@@ -21,8 +22,13 @@ if __name__ == '__main__':
         arr.append(list(map(int, input().rstrip().split())))
 
     k = int(input())
+
+    # sort arr according to attribute in column k
     arr.sort(key=lambda l:l[k])
-    # print(*map(lambda x: " ".join(x),list(map(lambda x: list(map(str,x)),arr))), sep='\n')
+
+    # print(*map(lambda x: " ".join(x),list(map(lambda x: list(map(str,x)),arr))), sep='\n') #solution using map func
+
+    # solution using nested loop
     for subArr in arr:
         for element in subArr:
             print(element,end=" ")
